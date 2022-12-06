2f71deccb260cc2a12e89037ec454c5c30634a6b
Chris Bright <89421730+InduPri48@users.noreply.github.com>
2022-12-06 14:00:28 +0000

    Added command line arguments

--- encode.py
+++ encode.py
@@ -1,117 +1,148 @@
-import sys
-import scipy.io.wavfile as wav
 from PIL import Image
 import numpy as np
+import scipy.io.wavfile as wav
+import scipy.signal as sps
+import sys
 
-def generate_carrier(length, amplitude=1000, fs=41600, freq=2400): # Rather arbitrary amplitude chosen here
+# Read the command line arguments: videoA location, videoB location, audio location. Optionally sample rate too
+args = sys.argv[1:]
+if len(args) < 3:
 
-    N = int(fs * length)
-    w = 2 * np.pi * freq
-    
-    t = np.linspace(0, length, N)
-
-    y = amplitude * np.sin(w * t)
-    
-    return fs, y
-
-# Some simple input validation for the two required image filepaths
-if not len(sys.argv) > 2:
-    
-    print("Incorrect parameters supplied")
-    exit()
-
-videoA_filepath, videoB_filepath = sys.argv[1:] # Get filepaths for both video files
-
-try:
-    
-    videoA = Image.open(videoA_filepath)
-    
-except:
-    
-    print("Could not load videoA image")
+    print("Not enough arguments provided")
     exit()
     
-try:
-    
-    videoB = Image.open(videoB_filepath)
-    
-except:
-    
-    print("Could not load videoB image")
-    exit()
+else:
 
-if videoA.width != 909:
-    
-    print("Video A does not have a width of 909px")
-    exit()
+    videoA_filepath = args[0]
+    videoB_filepath = args[1]
+    audio_filepath = args[2]
 
-if videoB.width != 909:
-    
-    print("Video B does not have a width of 909px")
-    exit()
+    if len(args) > 3:
 
-videoA_pixels = np.asarray(videoA)[:, :, 0] # Get the red pixel values for the video images as the images will be grayscale anyway
-videoB_pixels = np.asarray(videoB)[:, :, 0]
-
-syncA_pixels = np.array([0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,0], dtype=np.int8) * 255 # Generate a line of the sync line pixels
-syncB_pixels = np.array([0,0,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0], dtype=np.int8) * 255
-
-spaceA_pixels = np.zeros(47, dtype=np.int8) * 255 # Generate a line of the space pixels
-spaceB_pixels = np.ones(47, dtype=np.int8) * 255
-
-height = len(videoA_pixels) # Get the vertical height of the video images
-
-telemA_pixels = np.ones(45, dtype=np.int8) * 32 # Generate a blank space to fill the space where the telemetry data would be
-telemB_pixels = np.ones(45, dtype=np.int8) * 32
-
-image = np.concatenate((syncA_pixels, spaceB_pixels, videoA_pixels[0], telemA_pixels, syncB_pixels, spaceA_pixels, videoB_pixels[0], telemB_pixels)) # Create the first complete line of the final image
-    
-for i in range(1, height):
-
-    # This part essentially adds the minute markers in the spacing columns and a gradient pattern in the telemetry zones in accordance with the APT protocol
-    mm = i % 120 == 0 or i % 120 == 1
-
-    tm = (i >> 4) % 16
-
-    tm_col = 255
-    if tm < 8:
-
-        tm_col = 32 * (tm + 1)
-        tm_col = min(tm_col, 255)
-
-        telemA_pixels = np.ones(45, dtype=np.int8) * tm_col
-        telemB_pixels = np.ones(45, dtype=np.int8) * tm_col
-
-    elif tm == 8:
-
-        telemA_pixels = np.ones(45, dtype=np.int8) * 0
-        telemB_pixels = np.ones(45, dtype=np.int8) * 0
+        try:
+            output_sample_rate = int(args[3])
+        except:
+            print("Failed to parse the given sample rate into an integer") 
+            exit()
             
     else:
 
-        telemA_pixels = np.ones(45, dtype=np.int8) * 128
-        telemB_pixels = np.ones(45, dtype=np.int8) * 128
+        output_sample_rate = 11025
 
-    row = np.concatenate((syncA_pixels, spaceB_pixels if mm else spaceA_pixels, videoA_pixels[i], telemA_pixels, syncB_pixels, spaceA_pixels if mm else spaceB_pixels, videoB_pixels[i], telemB_pixels))
+# Load the video A and video B images
+try:
+    videoA_image = Image.open(videoA_filepath)
+except:
+
+   print("Failed to open video A image file")
+   exit()
+
+try:
+    videoB_image = Image.open(videoA_filepath)
+except:
+
+   print("Failed to open video B image file")
+   exit()
+   
+# Check to see if both images are 909px wide
+if videoA_image.width != 909:
+
+    print("Video A image is not 909px wide")
+    exit()
+
+if videoB_image.width != 909:
+
+    print("Video B image is not 909px wide")
+    exit()
+
+# Check to see if both images have the same height
+if videoA_image.height != videoB_image.height:
+
+    print("The two images do not have matching heights")
+    exit()
+
+# Convert the image to an 256 level array of each line of pixels i.e. a 2D array of array[row][col]. The red channel is chosen only.
+videoA_pixels = np.asarray(videoA_image)[:, :, 0]
+videoB_pixels = np.asarray(videoB_image)[:, :, 0]
+
+# Generate one line of the sync pixels
+syncA_pixels = np.array([0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,0], dtype=np.int8) * 255
+syncB_pixels = np.array([0,0,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0], dtype=np.int8) * 255
+
+# Generate one line of the space pixels
+spaceA_pixels = np.zeros(47, dtype=np.int8)
+spaceB_pixels = np.ones(47, dtype=np.int8) * 255
+
+# Create an empty array for our final image
+image_pixels = np.zeros(shape=(videoA_image.height, 2080))
+
+# Go through each line of pixels to create the final image
+for line in range(0, videoA_image.height):
+
+    minute_marker = line % 120 == 0 # will this line contain a minute marker in the spacings
+
+    # Telemetry
+    telemetry_block = (line // 8) % 16
+
+    if telemetry_block < 8:
+
+        block_color = 32 * (telemetry_block)
         
-    image = np.vstack((image, row)) # Shuffle the rows around to make a proper array ready to be converted into an image again
-    image = image.astype(np.uint8)
+        telemetryA_pixels = np.ones(45, dtype=np.int8) * block_color
+        telemetryB_pixels = np.ones(45, dtype=np.int8) * block_color
 
-Image.fromarray(image).save("image.png") # Save the final image before we modulate it upon the 2400Hz carrier
+    elif telemetry_block == 8:
 
-width = 2080 # Final image total width
-height = len(image) # Final image total height
+        telemetryA_pixels = np.zeros(45, dtype=np.int8)
+        telemetryB_pixels = np.zeros(45, dtype=np.int8)
 
-image = np.asarray(image).flatten() / 255 # Normalise the pixels from 0-255 to 0-1
+    else:
+        
+        telemetryA_pixels = np.ones(45, dtype=np.int8) * 128
+        telemetryB_pixels = np.ones(45, dtype=np.int8) * 128
+        
+    # stitch an entire row of the image together
+    row = np.concatenate((syncA_pixels, spaceB_pixels if minute_marker else spaceA_pixels, videoA_pixels[line], telemetryA_pixels, syncB_pixels, spaceA_pixels if minute_marker else spaceB_pixels, videoB_pixels[line], telemetryB_pixels))
 
-fs, carrier = generate_carrier(height * 0.5)
+    image_pixels[line] = row
+
+# Convert the pixel array back into a PIL image and save it
+image_pixels = image_pixels.astype(np.uint8)
+image = Image.fromarray(image_pixels)
+image_filepath = audio_filepath[:audio_filepath.index('.')] + '.png'
+image.save(image_filepath)
+
+# Flatten the image pixels array to a 1D array and normalize it
+image_pixels = np.asarray(image).flatten() / 255
+
+# Generate a carrier wave at 2400Hz
+sample_rate = 2080 * 20
+duration = 0.5 * image.height
+n_samples = int(duration * sample_rate)
+
+time = np.linspace(0, duration, n_samples)
+carrier = 1023 * np.sin(2 * np.pi * 2400 * time)
+
+# Scale up the signal to match the carrier sample number
+scale = n_samples // len(image_pixels)
+signal = np.repeat(image_pixels, scale)
+
+# Amplitude modulate the carrier with the 256-level signal
+modulated = carrier * signal
+
+# Resample the audio to the desired rate
+sample_rate = output_sample_rate
+n_samples = int(sample_rate * duration)
+modulated = sps.resample(modulated, n_samples)
+
+# Save the audio as a WAV file
+modulated = modulated.astype(np.int16)
+wav.write(audio_filepath, sample_rate, modulated)
+
+
+
+
+
+
+
     
-sl = int((fs * 0.5) / width) # How many samples of carrier will one pixel be encoded over
-    
-expanded = np.repeat(image, sl) # Blow up the image by the scale length factor so that the image pixel count and total carrier sample count match
-    
-modulated = carrier * expanded # Amplitude modulution here
-
-modulated = modulated.astype(np.int16) # Simple type change ready to be saved as WAV
-    
-wav.write("audio_file.wav", fs, modulated) # Save the modulated carrier as a WAV file
